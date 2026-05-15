fb6a771b9e5483d04b2a4ea6c8cf40487ec74e46
u2403230-hub <u2403230@rajagiri.edu.in>
2026-05-15 11:48:07 +0530

    Step 3: Added DeepSORT tracking

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, Response, render_template
 import cv2
 from ultralytics import YOLO
+from deep_sort_realtime.deepsort_tracker import DeepSort
 
 app = Flask(__name__)
 
 model = YOLO("yolov8n.pt")
+tracker = DeepSort()
 cap = cv2.VideoCapture("test.mp4")
 
 def generate_frames():
@@ -14,13 +16,26 @@ def generate_frames():
             break
 
         results = model(frame)
+        detections = []
 
         for r in results:
             for box in r.boxes:
                 cls = int(box.cls[0])
                 if cls == 0:
                     x1, y1, x2, y2 = map(int, box.xyxy[0])
-                    cv2.rectangle(frame, (x1,y1), (x2,y2), (0,255,0), 2)
+                    detections.append(([x1,y1,x2-x1,y2-y1], 0.9, 'person'))
+
+        tracks = tracker.update_tracks(detections, frame=frame)
+
+        for track in tracks:
+            if not track.is_confirmed():
+                continue
+            x1,y1,x2,y2 = map(int, track.to_ltrb())
+            track_id = track.track_id
+
+            cv2.rectangle(frame,(x1,y1),(x2,y2),(0,255,0),2)
+            cv2.putText(frame,f"ID {track_id}",(x1,y1-10),
+                        cv2.FONT_HERSHEY_SIMPLEX,0.6,(0,255,0),2)
 
         ret, buffer = cv2.imencode('.jpg', frame)
         frame = buffer.tobytes()
