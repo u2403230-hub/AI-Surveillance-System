71714e6a27225224aad757e2dcd6de0af6d7ce69
u2403230-hub <u2403230@rajagiri.edu.in>
2026-05-15 11:46:46 +0530

    Step 2: Added YOLOv8 detection

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, Response, render_template
 import cv2
+from ultralytics import YOLO
 
 app = Flask(__name__)
 
+model = YOLO("yolov8n.pt")
 cap = cv2.VideoCapture("test.mp4")
 
 def generate_frames():
@@ -10,12 +12,21 @@ def generate_frames():
         success, frame = cap.read()
         if not success:
             break
-        else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
 
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        results = model(frame)
+
+        for r in results:
+            for box in r.boxes:
+                cls = int(box.cls[0])
+                if cls == 0:
+                    x1, y1, x2, y2 = map(int, box.xyxy[0])
+                    cv2.rectangle(frame, (x1,y1), (x2,y2), (0,255,0), 2)
+
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/')
 def index():
@@ -26,5 +37,4 @@ def video():
     return Response(generate_frames(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
-if __name__ == "__main__":
-    app.run(debug=True)
+app.run(debug=True)
