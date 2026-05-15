37903fb56bf12b3c22db0449e050465697f65710
u2403230-hub <u2403230@rajagiri.edu.in>
2026-05-15 11:49:31 +0530

    Step 4: Intrusion detection added

--- app.py
+++ app.py
@@ -1,55 +1,9 @@
-from flask import Flask, Response, render_template
-import cv2
-from ultralytics import YOLO
-from deep_sort_realtime.deepsort_tracker import DeepSort
+zone_x1, zone_y1, zone_x2, zone_y2 = 200,100,500,400
+cx = (x1+x2)//2
+cy = (y1+y2)//2
 
-app = Flask(__name__)
+if zone_x1 < cx < zone_x2 and zone_y1 < cy < zone_y2:
+    cv2.putText(frame,"INTRUSION",(x1,y1-30),
+                cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
 
-model = YOLO("yolov8n.pt")
-tracker = DeepSort()
-cap = cv2.VideoCapture("test.mp4")
-
-def generate_frames():
-    while True:
-        success, frame = cap.read()
-        if not success:
-            break
-
-        results = model(frame)
-        detections = []
-
-        for r in results:
-            for box in r.boxes:
-                cls = int(box.cls[0])
-                if cls == 0:
-                    x1, y1, x2, y2 = map(int, box.xyxy[0])
-                    detections.append(([x1,y1,x2-x1,y2-y1], 0.9, 'person'))
-
-        tracks = tracker.update_tracks(detections, frame=frame)
-
-        for track in tracks:
-            if not track.is_confirmed():
-                continue
-            x1,y1,x2,y2 = map(int, track.to_ltrb())
-            track_id = track.track_id
-
-            cv2.rectangle(frame,(x1,y1),(x2,y2),(0,255,0),2)
-            cv2.putText(frame,f"ID {track_id}",(x1,y1-10),
-                        cv2.FONT_HERSHEY_SIMPLEX,0.6,(0,255,0),2)
-
-        ret, buffer = cv2.imencode('.jpg', frame)
-        frame = buffer.tobytes()
-
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/video')
-def video():
-    return Response(generate_frames(),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
-
-app.run(debug=True)
+cv2.rectangle(frame,(zone_x1,zone_y1),(zone_x2,zone_y2),(0,0,255),2)
